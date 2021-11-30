bcec5f54921d624192f85fc95c7b36d7f4585391
Abraam Ibrahim <87959474+EvoL1e@users.noreply.github.com>
2021-11-29 17:37:05 -0800

    Testing user creation through oauth

--- config/passport-setup.js
+++ config/passport-setup.js
@@ -28,10 +28,10 @@ new GoogleStrategy({
             console.log('user is: ', currentUser);
             done(null, currentUser);
         } else {
-            // if not, create user in our db
+            // if not, create user in our
             new GoogleUser({
                 googleId: profile.id,
-                username: profile.displayName,
+                name: profile.displayName,
             }).save().then((newUser) => {
                 console.log('created new user: ', newUser);
                 done(null, newUser);
